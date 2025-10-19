88633f3473988e43fdfc23baec296ec471bcb8aa
RekhuGopal <vrchinnarathod@gmail.com>
2025-10-19 23:22:49 +0530

    demo code

--- invoke_agent.py
+++ invoke_agent.py
@@ -3,11 +3,11 @@ import json
 
 agent_core_client = boto3.client('bedrock-agentcore', region_name='us-west-2')
 payload = json.dumps({
-    "input": {"prompt": "Explain machine learning in simple terms"}
+    "input": {"prompt": "Explain how does it rains"}
 })
 
 response = agent_core_client.invoke_agent_runtime(
-    agentRuntimeArn='arn:aws:bedrock-agentcore:us-west-2:357171621133:runtime/strands_agent_test-91MEErAlsQ',
+    agentRuntimeArn='arn:aws:bedrock-agentcore:us-west-2:357171621133:runtime/strands_agent_test-UpqRwVEzuY',
     runtimeSessionId='dfmeoagmreaklgmrkleafremoigrmtesogmtrskhmtkrlshmt',  # Must be 33+ chars
     payload=payload,
     qualifier="DEFAULT"
